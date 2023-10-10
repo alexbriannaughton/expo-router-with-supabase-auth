6b128304c25b814ac2929524c7725e99b3875188
Alex Brian Naughton <alexbriannaughton@gmail.com>
2023-10-09 20:56:27 -0700

    semicolons

--- app/index.js
+++ app/index.js
@@ -8,19 +8,19 @@ export default function IndexPage() {
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
-      if (session) router.replace("/(tabs)/home")
+      if (session) router.replace("/(tabs)/home");
     })
 
     supabase.auth.onAuthStateChange(async (event, session) => {
-      console.log(event)
+      console.log(event);
 
       if (event === 'PASSWORD_RECOVERY') {
-        console.log('hit the pw recovery event!')
+        console.log('hit the pw recovery event!');
         const { data, error } = await supabase.auth.updateUser({ password: 'fishfish' });
       }
 
-      if (session) router.replace("/(tabs)/home")
-      else router.replace("/(auth)/login")
+      if (session) router.replace("/(tabs)/home");
+      else router.replace("/(auth)/login");
     })
   }, [])
 
